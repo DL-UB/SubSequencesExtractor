0000ebde80f0247cc48c6fe27b8b0392a21772ec
DenisaConstantin <104455880+DenisaConstantin@users.noreply.github.com>
2024-09-19 13:19:56 +0300

    Update SubSequencesExtractor.sh

--- SubSequencesExtractor.sh
+++ SubSequencesExtractor.sh
@@ -18,8 +18,13 @@ rm -r seq*.fna *.fai
 
 sed -i 's/ /\t/g' coordinates_${chr%.fasta}.txt
 
-awk -F "\t" -v chrname="${chr%.fasta}" '{ FS = OFS = "\t" } {print "Coord",$0,chrname,"FALSE","plus","det","LOC","no","type","code","code1","no1"}' coordinates_${chr%.fasta}.txt > coord_tab_${chr%.fasta}.csv
+awk -F "\t" -v chrname="${chr%.fasta}" '{ FS = OFS = "\t" } {print "Coord",$0,chrname,"FALSE","plus","det","LOC","no","type","code","code1","no1"}' coordinates_${chr%.fasta}.txt > coord_tab_${chr%.fasta}.int.csv
 
 rm -r *.txt
 
 done
+
+cat *.fna > gene.fasta
+rm -r *.fna
+cat *.int.csv > coordinates_dataset.csv
+rm -r *.int.csv
