fbccbe617aef1b81a741a5980563b88b635de778
DenisaConstantin <104455880+DenisaConstantin@users.noreply.github.com>
2024-09-20 18:49:37 +0300

    Update SubSequencesExtractor.sh

--- SubSequencesExtractor.sh
+++ SubSequencesExtractor.sh
@@ -11,20 +11,20 @@ maximum=$(awk 'BEGIN {max = 0} {if ($2>max) max=$2} END {print max}' coordinates
 echo $maximum
 sed -i -e "s/$maximum/$length/" coordinates_${chr%.fasta}.txt
 
-cat coordinates_${chr%.fasta}.txt | while read start stop ; do seqkit subseq -r $start:$stop $chr > "seq_"$start".fna"; sed -i -e "s/>/\>Coord:$start-$stop /g" "seq_"$start".fna"; done
+cat coordinates_${chr%.fasta}.txt | while read start stop ; do seqkit subseq -r $start:$stop $chr > "seq_"$start".fna"; sed -i -e "s/>/\>Coord${chr%.fasta}:$start-$stop /g" "seq_"$start".fna"; done
 
 cat seq*.fna > all_seq_${chr%.fasta}.fna
 rm -r seq*.fna *.fai
 
 sed -i 's/ /\t/g' coordinates_${chr%.fasta}.txt
 
-awk -F "\t" -v chrname="${chr%.fasta}" '{ FS = OFS = "\t" } {print "Coord",$0,chrname,"FALSE","plus","det","LOC","no","type","code","code1","no1"}' coordinates_${chr%.fasta}.txt > coord_tab_${chr%.fasta}.int.csv
+awk -F "\t" -v chrname="${chr%.fasta}" '{ FS = OFS = "\t" } {print "Coord"chrname,$0,chrname,"FALSE","plus","det","LOC","no","type","code","code1","no1"}' coordinates_${chr%.fasta}.txt > coord_tab_${chr%.fasta}.int.csv
 
 rm -r *.txt
 
 done
 
-cat *.fna > gene.fasta
+cat *.fna > ranked_queries.fasta
 rm -r *.fna
 cat *.int.csv > coordinates_dataset.csv
 rm -r *.int.csv
